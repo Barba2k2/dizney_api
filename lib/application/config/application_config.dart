4db2baf8a617fbe9d753a84d10db994d51274ecc
Barba2k2 <lorenzo.dz@hotmail.com>
2024-05-06 15:13:11 -0300

    Core config updated

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,3 +1,29 @@
+import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:get_it/get_it.dart';
+
+import '../logger/i_logger.dart';
+import '../logger/logger.dart';
+import 'database_connection_configuration.dart';
+
 class ApplicationConfig {
-  void loadConfigApplication() async {}
+  Future<void> loadConfigApplication() async {
+    await _loadEnv();
+    _loadConfigDatabase();
+    _configLogger();
+  }
+
+  Future<void> _loadEnv() async => load();
+
+  void _loadConfigDatabase() {
+    final databseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databaseHost']!,
+      user: env['DATABASE_USER'] ?? env['databaseUser']!,
+      port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+    );
+    GetIt.I.registerSingleton(databseConfig);
+  }
+
+  void _configLogger() => GetIt.I.registerLazySingleton<ILogger>(() => TLogger());
 }
