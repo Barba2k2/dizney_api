100d63b6577a5a4ccc8213e3b2a1b979156a40d3
Barba2k2 <lorenzo.dz@hotmail.com>
2024-05-14 21:04:49 -0300

    REAFT: Refactoring views_models to match with controllers and database

--- lib/modules/user/view_models/login_view_model.dart
+++ lib/modules/user/view_models/login_view_model.dart
@@ -1,14 +1,18 @@
 import '../../../application/helpers/request_mapping.dart';
 
 class LoginViewModel extends RequestMapping {
-  late String login;
+  late String username; 
+  late String email; 
   late String password;
+  late String avatar; 
 
   LoginViewModel(String dataRequest) : super(dataRequest);
 
   @override
   void map() {
-    login = data['login'];
+    username = data['username'] ?? ''; 
+    email = data['email'] ?? ''; 
     password = data['password'];
+    avatar = data['avatar'];
   }
 }
--- lib/modules/user/view_models/user_save_input_model.dart
+++ lib/modules/user/view_models/user_save_input_model.dart
@@ -1,15 +1,70 @@
 import '../../../application/helpers/request_mapping.dart';
 
 class UserSaveInputModel extends RequestMapping {
+  late String firstname;
+  late String lastname;
+  late String username;
   late String email;
   late String password;
-  int? supplierId;
+  String? countryCode;
+  String? phoneCode;
+  String? phone;
+  String? address;
+  String? timezone;
+  String? image;
+  int? referralId;
+  int? languageId;
+  String? provider;
+  String? providerId;
+  int? status;
+  int? identityVerify;
+  int? addressVerify;
+  int? twoFa;
+  int? twoFaVerify;
+  String? twoFaCode;
+  int? emailVerification;
+  int? smsVerification;
+  String? verifyCode;
 
   UserSaveInputModel(String dataRequest) : super(dataRequest);
 
   @override
   void map() {
+    firstname = data['firstname'] ?? '';
+    lastname = data['lastname'] ?? '';
+    username = data['username'] ?? '';
     email = data['email'];
     password = data['password'];
+    countryCode = data['country_code'];
+    phoneCode = data['phone_code'];
+    phone = data['phone'];
+    address = data['address'];
+    timezone = data['timezone'];
+    image = data['image'];
+    referralId =
+        data['referral_id'] != null ? int.tryParse(data['referral_id']) : null;
+    languageId =
+        data['language_id'] != null ? int.tryParse(data['language_id']) : null;
+    provider = data['provider'];
+    providerId = data['provider_id'];
+    status = data['status'] != null ? int.tryParse(data['status']) : null;
+    identityVerify = data['identity_verify'] != null
+        ? int.tryParse(data['identity_verify'])
+        : null;
+    addressVerify = data['address_verify'] != null
+        ? int.tryParse(data['address_verify'])
+        : null;
+    twoFa = data['two_fa'] != null ? int.tryParse(data['two_fa']) : null;
+    twoFaVerify = data['two_fa_verify'] != null
+        ? int.tryParse(data['two_fa_verify'])
+        : null;
+    twoFaCode = data['two_fa_code'];
+    emailVerification = data['email_verification'] != null
+        ? int.tryParse(data['email_verification'])
+        : null;
+    smsVerification = data['sms_verification'] != null
+        ? int.tryParse(data['sms_verification'])
+        : null;
+    verifyCode = data['verify_code'];
   }
 }
